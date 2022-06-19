3594f1312ed6a7be5d2f221858af429c8cd440e1
Igor-Jacinto <telesigornm@gmail.com>
2022-06-19 14:05:53 -0400

    “organizando-detalhes”

--- src/Title.jsx
+++ src/Title.jsx
@@ -2,7 +2,7 @@ import "./Novo.css"
 function Title(){
     return(
        < div>
-            <p className="title"> NOTÍCIAS</p>
+            <p className="title"> 📶 NOTÍCIAS</p>
             <p className="subtitle">PORTAL DO IFRO</p>
        
        </div>
